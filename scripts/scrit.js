83fee61805056a4cc793c6e90b5f1d79685876a4
JokerJeans <vg154476@gmail.com>
2024-05-24 21:58:42 -0300

    animações no menu

--- scripts/scrit.js
+++ scripts/scrit.js
@@ -1,13 +1,31 @@
 const menu_button_element = document.getElementById("menu_button");
-
+const menu_element = document.getElementById("menu");
 menu_button_element.addEventListener("click", () => {
-    const menu_element = document.getElementById("menu");
-
-    let menuVisibility = Boolean(menu_element.style.display === "none")
-
-    if(menuVisibility){
-        menu_element.style.display = "block"
-    } else {
-        menu_element.style.display = "none"
+    if(!menuVisible){
+        showMenu()
+    } else{
+        hideMenu()
     }
-})
+});
+
+let menuVisible = false;
+
+function hideMenu() {
+  menu_element.classList.remove("slideMenuIn");
+  menu_element.classList.add("slideMenuBack");
+  menu_element.style.display = "block"
+
+  menuVisible = false;
+
+  return menuVisible;
+}
+
+function showMenu() {
+  menu_element.classList.remove("slideMenuBack");
+  menu_element.classList.add("slideMenuIn");
+  menu_element.style.display = "block";
+
+  menuVisible = true;
+
+  return menuVisible;
+}
