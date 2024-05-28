44724333f431fe89cf4aa8d631bbe412662517cf
JokerJeans <vg154476@gmail.com>
2024-05-27 23:55:01 -0300

    atualizações header

--- scripts/scrit.js
+++ scripts/scrit.js
@@ -1,13 +1,13 @@
 const menu_button_element = document.getElementById("menu_button");
 const menu_element = document.getElementById("menu");
+const main_element = document.getElementById("main");
 
-if(window.innerWidth < 401){
 menu_button_element.addEventListener("click", () => {
-    if(!menuVisible){
-        showMenu()
-    } else{
-        hideMenu()
-    }
+  if (!menuVisible) {
+    showMenu();
+  } else {
+    hideMenu();
+  }
 });
 
 let menuVisible = false;
@@ -15,7 +15,7 @@ let menuVisible = false;
 function hideMenu() {
   menu_element.classList.remove("slideMenuIn");
   menu_element.classList.add("slideMenuBack");
-  menu_element.style.display = "block"
+  menu_element.style.display = "none";
 
   menuVisible = false;
 
@@ -31,12 +31,41 @@ function showMenu() {
 
   return menuVisible;
 }
-} else {
-  menu_button_element.addEventListener("click", ()=> {
-    if(menu_element.style.display === "block") {
-      menu_element.style.display = "none"
-    }else{
-      menu_element.style.display = "block"
-    }
-  })
+
+function showHeaderBox(scrollPos) {
+  const header_box_element = document.getElementById("conteiner-header");
+  if (scrollPos > 199) {
+    header_box_element.style.opacity = "1"
+  } else {
+    header_box_element.style.opacity = "0";
+  }
 }
+
+let lastPos = 0;
+let ticking = false;
+
+document.addEventListener("scroll", () => {
+  lastPos = window.scrollY;
+  console.log(lastPos);
+
+  if (!ticking) {
+    window.requestAnimationFrame(() => {
+      showHeaderBox(lastPos);
+      ticking = false;
+    });
+  }
+  ticking = true;
+});
+
+/* else {
+  menu_button_element.addEventListener("click", () => {
+    if (menu_element.style.display === "block") {
+      menu_element.classList.remove("slideMenuIn");
+      menu_element.classList.add("slideMenuBack");
+      menu_element.style.display = "block";
+    } else {
+      menu_element.classList.remove("slideMenuBack");
+      menu_element.classList.add("slideMenuIn");
+      menu_element.style.display = "block";
+    }
+  }); */
