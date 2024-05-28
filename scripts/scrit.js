10bb7d4e891c833b4f2f4edba66e31ef039c3d49
JokerJeans <vg154476@gmail.com>
2024-05-28 11:48:56 -0300

    atualizações Menu

--- scripts/scrit.js
+++ scripts/scrit.js
@@ -4,25 +4,55 @@ const main_element = document.getElementById("main");
 
 menu_button_element.addEventListener("click", () => {
   if (!menuVisible) {
+
     showMenu();
+    
+    
   } else {
     hideMenu();
+   
   }
 });
 
 let menuVisible = false;
 
+function waitingAnimationEnd(windowWidth) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      if(windowWidth > 500){
+        resolve("none");
+
+      }else{
+        
+        resolve("block");
+      }
+
+    }, 400);
+
+  })
+}
+
+async function resultFunction() {
+  const result = await waitingAnimationEnd(window.innerWidth);
+  menu_element.style.display = result;
+}
+
+
+
 function hideMenu() {
   menu_element.classList.remove("slideMenuIn");
   menu_element.classList.add("slideMenuBack");
-  menu_element.style.display = "none";
-
+  
+  resultFunction();
+  
   menuVisible = false;
 
   return menuVisible;
+  
 }
 
 function showMenu() {
+  
   menu_element.classList.remove("slideMenuBack");
   menu_element.classList.add("slideMenuIn");
   menu_element.style.display = "block";
@@ -35,10 +65,17 @@ function showMenu() {
 function showHeaderBox(scrollPos) {
   const header_box_element = document.getElementById("conteiner-header");
   if (scrollPos > 199) {
-    header_box_element.style.opacity = "1"
+    header_box_element.style.opacity = "1";
   } else {
     header_box_element.style.opacity = "0";
   }
+
+  if (scrollPos < 130) {
+    header_box_element.style.display = "none";
+    return;
+  }
+
+  header_box_element.style.display = "flex";
 }
 
 let lastPos = 0;
