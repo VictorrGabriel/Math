cd625bb766cfe027d55dc9c39d1f7319c4fbc08b
JokerJeans <vg154476@gmail.com>
2024-05-30 02:06:00 -0300

    atualização mobile: Menu Position

--- scripts/scrit.js
+++ scripts/scrit.js
@@ -1,16 +1,17 @@
 const menu_button_element = document.getElementById("menu_button");
 const menu_element = document.getElementById("menu");
 const main_element = document.getElementById("main");
+const close_menu_element = document.getElementById("close_menu");
+
+close_menu_element.addEventListener("click", () => {
+  hideMenu();
+});
 
 menu_button_element.addEventListener("click", () => {
   if (!menuVisible) {
-
     showMenu();
-    
-    
   } else {
     hideMenu();
-   
   }
 });
 
@@ -19,17 +20,13 @@ let menuVisible = false;
 function waitingAnimationEnd(windowWidth) {
   return new Promise((resolve) => {
     setTimeout(() => {
-      if(windowWidth > 500){
+      if (windowWidth > 500) {
         resolve("none");
-
-      }else{
-        
+      } else {
         resolve("block");
       }
-
     }, 400);
-
-  })
+  });
 }
 
 async function resultFunction() {
@@ -37,22 +34,18 @@ async function resultFunction() {
   menu_element.style.display = result;
 }
 
-
-
 function hideMenu() {
   menu_element.classList.remove("slideMenuIn");
   menu_element.classList.add("slideMenuBack");
-  
+
   resultFunction();
-  
+
   menuVisible = false;
 
   return menuVisible;
-  
 }
 
 function showMenu() {
-  
   menu_element.classList.remove("slideMenuBack");
   menu_element.classList.add("slideMenuIn");
   menu_element.style.display = "block";
@@ -64,19 +57,17 @@ function showMenu() {
 
 function showHeaderBox(scrollPos) {
   const header_box_element = document.getElementById("conteiner-header");
-  const menu_icon_element = document.getElementById("menu_button")
   if (scrollPos > 199) {
     header_box_element.style.opacity = "1";
+    menu_button_element.style.display = "inline";
   } else {
     header_box_element.style.opacity = "0";
+    menu_button_element.style.display = "none";
   }
 
-  if (scrollPos < 190) {
-    menu_icon_element.display = "none";
-    return;
-  }
-
-  menu_icon_element.display = "block";
+  /* if (scrollPos < 199) {
+    
+  }  */
 }
 
 let lastPos = 0;
@@ -84,7 +75,6 @@ let ticking = false;
 
 document.addEventListener("scroll", () => {
   lastPos = window.scrollY;
-  console.log(lastPos);
 
   if (!ticking) {
     window.requestAnimationFrame(() => {
